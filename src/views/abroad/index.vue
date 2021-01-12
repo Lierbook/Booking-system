5fe27a666e604fecfbef59e85d510a4882ec06ba
PeachCx330 <1656267032@qq.com>
2021-01-12 10:31:27 +0800

    首次提交

---
+++ src/views/abroad/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    商品列表
+  </div>
+</template>
+<script>
+import { getAbroadList } from "@/api/abroad"
+export default {
+  data() {
+    return {
+      list:[]
+    };
+  },
+  created() {
+    this.getAbList();
+  },
+  methods: {
+    async getAbList(){
+      let result=await getAbroadList();
+      console.log(result);
+    }
+  },
+}
+</script>
---
+++ src/views/domestic/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    国内航班
+  </div>
+</template>
+<script>
+import { getAbroadList } from "@/api/abroad"
+export default {
+  data() {
+    return {
+      list:[]
+    };
+  },
+  created() {
+    this.getAbList();
+  },
+  methods: {
+    async getAbList(){
+      let result=await getAbroadList();
+      console.log(result);
+    }
+  },
+}
+</script>
